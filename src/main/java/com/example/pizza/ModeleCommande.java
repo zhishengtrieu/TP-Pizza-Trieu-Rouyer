350c8e55f15f4d8cd7897bc1c585893f14bfd932
Hugoll-1 <101283981+Hugoll-1@users.noreply.github.com>
2022-11-16 11:39:54 +0100

    Modification de la classe ModeleCommande pour prendre en compte la fidelite du client

--- src/main/java/com/example/pizza/ModeleCommande.java
+++ src/main/java/com/example/pizza/ModeleCommande.java
@@ -12,7 +12,12 @@ public class ModeleCommande {
     private ArrayList<Pizza> listPizza;
     private int numCommande;
     private double prixCommande;
+    public double taux;
 
+    /**
+     * Constructeur de la classe ModeleCommande
+     * @param numCommande
+     */
     public ModeleCommande(int numCommande) {
         this.nbPizza = 0;
         this.listPizza = new ArrayList<Pizza>();
@@ -20,7 +25,24 @@ public class ModeleCommande {
         this.prixCommande = 0;
     }
 
+    /**
+     * Met à jour le taux de réduction en fonction du type de client
+     * @param strategyFidelite
+     */
+    public void setTaux (StrategyFidelite strategyFidelite) {
+        if (strategyFidelite != null) {
+            this.taux = strategyFidelite.getTaux();
+        } else {
+            this.taux = 1.0;
+        }
+    }
+
+    /**
+     * Ajoute une pizza à la commande
+     * @param pizza
+     */
     public void ajouterPizza (String pizza){
+        //On vérifie que le nombre max de pizza n'est pas atteint pour une commande
         if (nbPizza < 4){
             Pizza p;
             if (pizza.compareTo("Creme") == 0){
@@ -34,26 +56,45 @@ public class ModeleCommande {
         }
     }
 
+    /**
+     * Methode permettant de calculer le prix de la commande
+     */
     public void calculerPrixCommande(){
         double prix = 0;
         for (Pizza p : listPizza){
-            prix += p.cout();
+            prix += p.cout() * taux;
         }
         prixCommande = prix;
     }
 
+    /**
+     * Retourne le nombre de pizza de la commande
+     * @return
+     */
     public int getNbPizza() {
         return listPizza.size();
     }
 
+    /**
+     * Retourne le prix de la commande
+     * @return
+     */
     public double getPrixCommande() {
         return prixCommande;
     }
 
+    /**
+     * Retourne la liste de pizza
+     * @return
+     */
     public ArrayList<Pizza> getListPizza() {
         return listPizza;
     }
 
+    /**
+     * Retourne le numéro de la pizza courante
+     * @return
+     */
     public int getNbPizzaCourante() {
         return nbPizza;
     }
